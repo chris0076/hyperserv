cb903243e319c1a5d005a48ac6fe985fdd188a8f
Alexandru Stan <alex@alex-laptop.(none)>
2010-06-17 15:11:54 -0400

    80% working parser, but rewrite needed

--- cubescript.py
+++ cubescript.py
@@ -1,34 +1,80 @@
 #!/usr/bin/env python
+import re
 
 #string="+ 1 2"
-string="+ (+ 4 3) (- 4 5) 2"
+string='+ (+ 433   [+ 1 "alex test  //test"] 9 3) (- 4 5 ) 2'
+
+string="""
+if $blendpaintmode [scrollblendbrush $arg1
+] [
+editfacewentpush $arg1 1 // Fill and Empty cube
+]
+"""
+
+quotefinder=re.compile("\"")
+
+def consume(length=1):
+	global string
+	string=string[length:]
 
 def parse():
-	global string;
-	output=[]
+	global string
+	output=[""]
 	
-	while 1:
+	while True:
 		print string
+		print "output", output
 		if string.startswith("("):
-			string=string[1:]
-			output.append(parse())
+			consume()
+			if not isinstance(output[-1], str):
+				output.append("")
+			output[-1]=parse()
 		elif string.startswith(")"):
-			string=string[1:]
-			return output[:-1]
+			consume()
+			break
 		
 		elif string.startswith("["):
-			pass
+			consume()
+			if not isinstance(output[-1], str):
+				output.append("")
+			output[-1]=["lambda",""]
+			output[-1][1]=parse()
+		elif string.startswith("]"):
+			consume()
+			break
+		
 		elif string.startswith("\""):
-			pass
-		elif string.startswith(" "):
-			output.append("")
-			string=string[1:]
+			position=quotefinder.search(string[1:]).start()+2
+			output[-1]=string[:position]
+			consume(position)
+		
+		elif string.startswith(" ") or string.startswith("\t"):
+			consume()
+			if output[-1]!="":
+				output.append("")
+		
+		elif string.startswith(";") or string.startswith("\n"):
+			consume()
+			if output!=[""]:
+				if output[-1]=="":
+					output=output[:-1]
+				output=["begin",output,parse()]
+				if output[2]==[]:
+					output=output[1]
+		
+		elif string.startswith("//"):
+			string=string.partition("\n")[2]
+		
 		elif string=="":
 			break
 		else:
-			if len(output)==0:
+			if not isinstance(output[-1], str):
 				output.append("")
-			output[-1],string=string[0],string[1:]
+			output[-1]=output[-1]+string[0]
+			consume()
+			
+	if output[-1]=="":
+		output=output[:-1]
 	return output
 
 #print add(['1','2'])
