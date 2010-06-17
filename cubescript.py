d51aa72ad7df1616b07850089a43f09686a4f266
Alexandru Stan <alex@alex-laptop.(none)>
2010-06-17 13:17:28 -0400

    trying some homebrew parser

--- cubescript.py
+++ cubescript.py
@@ -1,83 +1,37 @@
 #!/usr/bin/env python
 
-from sexpr import str2sexpr
+#string="+ 1 2"
+string="+ (+ 4 3) (- 4 5) 2"
 
-#def multisplit(s, seps):
-	#"""Splits with multiple separators"""
-	#res = [s]
-	#for sep in seps:
-		#s, res = res, []
-		#for seq in s:
-			#res += seq.split(sep)
-	#return res
-
-#def parse(command):
-	#command=command.strip();
-	#instructions=command.split(";")
-	#if(command==""):
-		#return ""
-	##elif (command=="")
-	#return instructions;
+def parse():
+	global string;
+	output=[]
 	
-	#return ""
-
-def tokenize(command):
-	tokens=[]
-	command=command.strip(" ")
-	while(command!=""):
-		if(command[0]=="("):
-			partition=command.partition(")")
-			tokens.append(partition[0][1:])
-			command=command[2]
-		elif(command[0]=="["):
-			partition=command.partition("]")
-			tokens.append(partition[0][1:])
-			command=command[2]
-		elif(command[0]=="\""):
-			partition=command.partition("\"")
-			tokens.append(partition[0][1:])
-			command=command[2]
+	while 1:
+		print string
+		if string.startswith("("):
+			string=string[1:]
+			output.append(parse())
+		elif string.startswith(")"):
+			string=string[1:]
+			return output[:-1]
+		
+		elif string.startswith("["):
+			pass
+		elif string.startswith("\""):
+			pass
+		elif string.startswith(" "):
+			output.append("")
+			string=string[1:]
+		elif string=="":
+			break
 		else:
-			partition=command.partition(" ")
-			tokens.append(partition[0])
-			command=command[2]
-		command=command.strip(" ")
-	return tokens
-
-def parse(command):
-	#Split into tokens
-	if command[0]=="(":
-		if command[-1]!=")":
-			print "Expected )"
-		else:
-			command=command[1:-1]
-	if command[0]=="[":
-		if command[-1]!="]":
-			print "Expected ]"
-		else:
-			command=command[1:-1]
-	tokens=command.split();
-	
-	#prepare
-	function,parameters=tokens[0],tokens[1:]
-	
-	#make sure everything is redox
-	for i in range(0,len(parameters)):
-		if parameters[i].startswith("(") or parameters[i].startswith("["):
-			parameters[i]=parse(parameters[i])
-	
-	#do the function
-	if(function=="+"):
-		return add(parameters)
-	else: print "Unknown function "+function
-	return 0
-
-def add(parameters):
-	return sum(map(int,parameters));
-
-print str2sexpr("(+ 1; + 1 2 [+ 3 4])")
+			if len(output)==0:
+				output.append("")
+			output[-1],string=string[0],string[1:]
+	return output
 
 #print add(['1','2'])
-#print parse("(+ 1 2)");
+print parse();
 #print tokenize("(+ 1 (+ 2 3))");
 #print parse(" spectator 1 1; spectator 0 1; test ")
