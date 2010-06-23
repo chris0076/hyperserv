63bb6ae978fef91bdd3a80e5bdaacf2de71d5169
Alexandru Stan <alex@alex-desktop.(none)>
2010-06-22 20:20:38 -0400

    removed all python files except the main loader, server still works but it does not allow anything

--- src/pyscripts/pygeoip/geoip.py
+++
@@ -1,8 +0,0 @@
-db = pygeoip.GeoIP('pygeoip/GeoIP.dat')
-
-def getCountry(ip): 
-	country = db.country_name_by_addr(ipLongToString(ip))
-	if country == '':
-		country = 'Unknown'
-	return country
-
--- src/pyscripts/xsbs/plugins.py
+++ src/pyscripts/xsbs/plugins.py
@@ -1,35 +1,25 @@
-from elixir import setup_all, create_all
 import os, sys
 
 # Initialize these before loading plugins
-import xsbs.db
-import xsbs.events
-import xsbs.log
-import xsbs.ban
-import xsbs.users
-import xsbs.server
-import xsbs.game
-import xsbs.teamcontrol
-import xsbs.persistteam
-import xsbs.demo
-import xsbs.http
-import xsbs.http.jsonapi
 
 class PluginManager(object):
 	def __init__(self, plugins_path='plugins'):
-		self.plugins_path = plugins_path
-		self.plugin_modules = []
+		#self.plugins_path = plugins_path
+		#self.plugin_modules = []
+		pass
 	def loadPlugins(self):
-		files = os.listdir(self.plugins_path)
-		for file in files:
-			if file[0] != '.':
-				self.plugin_modules.append(__import__(os.path.basename(self.plugins_path) + '.' + os.path.splitext(file)[0]))
+		#files = os.listdir(self.plugins_path)
+		#for file in files:
+		#	if file[0] != '.':
+		#		self.plugin_modules.append(__import__(os.path.basename(self.plugins_path) + '.' + os.path.splitext(file)[0]))
+		pass
 
 def main():
-	pm = PluginManager()
-	pm.loadPlugins()
-	setup_all()
-	create_all()
+	#pm = PluginManager()
+	#pm.loadPlugins()
+	#setup_all()
+	#create_all()
+	pass
 
 main()
 
