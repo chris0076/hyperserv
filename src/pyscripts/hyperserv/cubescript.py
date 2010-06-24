38dd9f4a7483f5d74f1bf005ee18ad5bd21d87e7
Alexandru Stan <alex@alex-desktop.(none)>
2010-06-23 23:28:05 -0400

    2 instances of the interpreter implemented, but not working because the outside 'functions' variable gets functions defined for the one class only, the lambda in the functionwrapper is at fault, since it encapsulates 'this'

--- src/pyscripts/hyperserv/cubescript.py
+++ src/pyscripts/hyperserv/cubescript.py
@@ -11,14 +11,22 @@ def PlayerMessage(a,b):
 			playerCS.executeby(a,"echo Error: "+' '.join(e))
 
 class CSInterpreterOwner(CSInterpreter):
-	owner=""
 	def executeby(self,owner,string):
 		self.owner=owner
-		print "setting owner to",owner
+		print "setting owner to",self.owner
 		return self.executestring(string)
+		
+def whoami(interpreter):
+	print interpreter.owner
 
-systemCS=CSInterpreterOwner()
-systemCS.addfunction("outputfunction",lambda interpreter,msg: sbserver.message(msg))
-systemCS.addfunction("map",lambda interpreter,name,mode=1: sbserver.setMap(name,mode))
-systemCS.addfunction("whoami",lambda interpreter: "cs"+str(interpreter.owner))
-playerCS=CSInterpreterOwner(systemCS.functions,systemCS.variables)
+playerCS=CSInterpreterOwner()
+playerCS.addfunction("outputfunction",lambda interpreter,msg: sbserver.message(msg))
+playerCS.addfunction("map",lambda interpreter,name,mode=1: sbserver.setMap(name,mode))
+playerCS.addfunction("whoami",whoami)
+
+systemCS=CSInterpreterOwner(playerCS.functions.copy(),playerCS.variables)
+systemCS.owner=("system","")
+
+
+playerCS.executeby(("ingame",0),"echo (whoami)")
+systemCS.executestring("echo (whoami)")
