cf9e28d9b395151cbd3d704aa5628a2c90ba4309
Alexandru Stan <alex@alex-desktop.(none)>
2010-07-30 20:20:25 -0400

    added sql automatic reconnect

--- src/pyscripts/hypershade/userdatabase.py
+++ src/pyscripts/hypershade/userdatabase.py
@@ -1,16 +1,20 @@
 from hypershade.config import config
 
+
+if config["dbtype"]=="mysql":
+	import MySQLdb as sqlmodule
+elif config["dbtype"]=="sqlite":
+	import sqlite3 as sqlmodule
+else:
+	raise Exception("Database driver %s not implemented." % config["dbtype"])
+
 class UserDatabase():
 	def __init__(self):
 		if config["dbtype"]=="mysql":
-			import MySQLdb
-			self.connection=MySQLdb.connect(host=config["dbhost"],user=config["dbuser"],passwd=config["dbpassword"],db=config["dbname"])
+			self.connection=sqlmodule.connect(host=config["dbhost"],user=config["dbuser"],passwd=config["dbpassword"],db=config["dbname"])
 		elif config["dbtype"]=="sqlite":
-			import sqlite3
-			self.connection = sqlite3.connect(config["dburl"])
+			self.connection = sqlmodule.connect(config["dburl"])
 			self.connection.text_factory = str
-		else:
-			raise Exception("Database driver %s not implemented." % config["dbtype"])
 		self.cursor=self.connection.cursor()
 	
 	def __iter__(self):
@@ -18,11 +22,11 @@ class UserDatabase():
 			yield user[0]
 	
 	def items(self):
-		self.cursor.execute('SELECT * FROM `users` where `key` = "privileges"')
+		self.query('SELECT * FROM `users` where `key` = "privileges"')
 		return tuple(self.cursor.fetchall())
 	
 	def __getitem__(self,username):
-		self.cursor.execute('SELECT `user` FROM `users` WHERE `user` = "%s" AND `key` = "privileges"' % (username))
+		self.query('SELECT `user` FROM `users` WHERE `user` = "%s" AND `key` = "privileges"' % (username))
 		
 		if self.cursor.rowcount==0:
 			raise KeyError("No such user: %s" % (username))
@@ -37,24 +41,30 @@ class UserDatabase():
 		User(self,username)["privileges"]=privileges
 	
 	def __delitem__(self,username):
-		self.cursor.execute('DELETE FROM `users` WHERE `user` = "%s"' % (username))
-		self.connection.commit()
+		self.query('DELETE FROM `users` WHERE `user` = "%s"' % (username))
 	
 	def __repr__(self):
 		return repr(self.items())
 	
 	def search(self,key,value):
-		self.cursor.execute('SELECT `user` FROM `users` WHERE `key` = "%s" and `value` = "%s"' % (key, value))
+		self.query('SELECT `user` FROM `users` WHERE `key` = "%s" and `value` = "%s"' % (key, value))
 		result=self.cursor.fetchone()
 		if result is not None:
 			return self[result[0]]
 		else:
 			return None
+	
+	def query(self,*args):
+		try:
+			self.cursor.execute(*args)
+		except sqlmodule.OperationalError:
+			self.__init__() #reconnect to the server
+			self.cursor.execute(*args)
+		self.connection.commit()
 
 class User():
-	def __init__(self,parent,username):
-		self.cursor=parent.cursor
-		self.connection=parent.connection
+	def __init__(self,database,username):
+		self.database=database
 		self.username=username
 	
 	def __iter__(self):
@@ -62,12 +72,12 @@ class User():
 			yield row[0]
 	
 	def items(self):
-		self.cursor.execute('SELECT `key`,`value` FROM `users` WHERE `user` = "%s"' % (self.username))
-		return (("username",self.username),)+tuple(self.cursor.fetchall())
+		self.database.query('SELECT `key`,`value` FROM `users` WHERE `user` = "%s"' % (self.username))
+		return (("username",self.username),)+tuple(self.database.cursor.fetchall())
 	
 	def __getitem__(self,key):
-		self.cursor.execute('SELECT value FROM `users` WHERE `user` = "%s" AND `key` = "%s"' % (self.username,key))
-		return tuple(row[0] for row in self.cursor.fetchall())
+		self.database.query('SELECT value FROM `users` WHERE `user` = "%s" AND `key` = "%s"' % (self.username,key))
+		return tuple(row[0] for row in self.database.cursor.fetchall())
 	
 	def __setitem__(self,key,values):
 		del self[key] #delete the old values
@@ -75,12 +85,10 @@ class User():
 			#probably a mistake, just one value is wanted
 			values=(values,)
 		for value in values:
-			self.cursor.execute('INSERT INTO `users` VALUES ("%s","%s","%s")' % (self.username,key,value))
-			self.connection.commit()
+			self.database.query('INSERT INTO `users` VALUES ("%s","%s","%s")' % (self.username,key,value))
 	
 	def __delitem__(self, key):
-		self.cursor.execute('DELETE FROM `users` WHERE `user` = "%s" AND `key` = "%s"' % (self.username,key))
-		self.connection.commit()
+		self.database.query('DELETE FROM `users` WHERE `user` = "%s" AND `key` = "%s"' % (self.username,key))
 	
 	def __repr__(self):
 		return repr(self.items())
